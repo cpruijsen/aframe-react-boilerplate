3b96f0485f53c645067652aaf7dfb7f56b80342b
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-07-07 13:55:02 -0700

    dynamic component rendering and dataJSON added

---
+++ src/data/data.js
@@ -0,0 +1,213 @@
+var data = [
+    {
+      "id": 232,
+      "name": "Alex Stark",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344522947.jpg"
+    },
+    {
+      "id": 234,
+      "name": "Andrew Heath",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344530415.jpg"
+    },
+    {
+      "id": 235,
+      "name": "Billy Lan",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344537377.jpg"
+    },
+    {
+      "id": 236,
+      "name": "Carl Chen",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344543688.jpg"
+    },
+    {
+      "id": 237,
+      "name": "Charlie Tran",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344551489.jpg"
+    },
+    {
+      "id": 239,
+      "name": "Chris Pruijsen",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344558205.jpg"
+    },
+    {
+      "id": 240,
+      "name": "Clay Han",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344565055.jpg"
+    },
+    {
+      "id": 241,
+      "name": "Derek Hoang",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344571037.jpg"
+    },
+    {
+      "id": 242,
+      "name": "Edmund To",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344577671.jpg"
+    },
+    {
+      "id": 243,
+      "name": "Frances Yang",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344584610.jpg"
+    },
+    {
+      "id": 244,
+      "name": "Frank Lee",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344590855.jpg"
+    },
+    {
+      "id": 245,
+      "name": "Greg Roche",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344604186.jpg"
+    },
+    {
+      "id": 246,
+      "name": "Hannah Brannan",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344611297.jpg"
+    },
+    {
+      "id": 247,
+      "name": "Hien Tran",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344618458.jpg"
+    },
+    {
+      "id": 366,
+      "name": "Hongchao Yu",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344635550.jpg"
+    },
+    {
+      "id": 248,
+      "name": "Jae Shin",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344703293.jpg"
+    },
+    {
+      "id": 249,
+      "name": "Jason Davenport",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344710704.jpg"
+    },
+    {
+      "id": 250,
+      "name": "Jeff Korcal",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344719125.jpg"
+    },
+    {
+      "id": 251,
+      "name": "Jeff Leu",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344727745.jpg"
+    },
+    {
+      "id": 367,
+      "name": "Jessica Chiou",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344736191.jpg"
+    },
+    {
+      "id": 252,
+      "name": "Joann Im",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344744147.jpg"
+    },
+    {
+      "id": 255,
+      "name": "Julius Buckley",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344771521.jpg"
+    },
+    {
+      "id": 256,
+      "name": "June Won",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344779776.jpg"
+    },
+    {
+      "id": 257,
+      "name": "Justin Ou",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344788003.jpg"
+    },
+    {
+      "id": 258,
+      "name": "Keith Wong",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344796474.jpg"
+    },
+    {
+      "id": 259,
+      "name": "Krishan Arya",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344804672.jpg"
+    },
+    {
+      "id": 260,
+      "name": "Leah Loversky",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344814941.jpg"
+    },
+    {
+      "id": 261,
+      "name": "Ling Chen",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344823439.jpg"
+    },
+    {
+      "id": 263,
+      "name": "Matt Aftalion",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344832652.jpg"
+    },
+    {
+      "id": 264,
+      "name": "Michael De La Cruz",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344841515.jpg"
+    },
+    {
+      "id": 265,
+      "name": "Michelle Nguyen",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344849833.jpg"
+    },
+    {
+      "id": 368,
+      "name": "Pranay Mohan",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344858400.jpg"
+    },
+    {
+      "id": 266,
+      "name": "Pyry Kovanen",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344870319.jpg"
+    },
+    {
+      "id": 369,
+      "name": "Rafael Espinoza",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344879231.jpg"
+    },
+    {
+      "id": 267,
+      "name": "Robb Marsee",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344888716.jpg"
+    },
+    {
+      "id": 268,
+      "name": "Ryan Hanzawa",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344899015.jpg"
+    },
+    {
+      "id": 370,
+      "name": "Ryan McChesney",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344908139.jpg"
+    },
+    {
+      "id": 270,
+      "name": "Shez Daredia",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465344933514.jpg"
+    },
+    {
+      "id": 272,
+      "name": "Vinny Huang",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465345040866.jpg"
+    },
+    {
+      "id": 273,
+      "name": "Yuyi Li",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465345051804.jpg"
+    },
+    {
+      "id": 262,
+      "name": "Zohar Sanchez",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465345065272.jpg"
+    },
+    {
+      "id": 238,
+      "name": "Chris Walker",
+      "image": "https://www.hackreactorconnect.com/assets/photos/1465503991471.jpg"
+    }
+  ];
+export default data;
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import 'babel-polyfill';
 import {Animation, Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import data from '../data/data';
 
 import Camera from './components/Camera';
 import Cursor from './components/Cursor';
@@ -24,6 +25,7 @@ class BoilerplateScene extends React.Component {
   };
 
   render () {
+    var that = this;
     return (
       <Scene>
         <Camera><Cursor/></Camera>
@@ -34,14 +36,27 @@ class BoilerplateScene extends React.Component {
         <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
         <Entity light={{type: 'directional', intensity: 1}} position={[1, 1, 0]}/>
 
+        {/* the below geo element should be templated to data
         <Entity geometry="primitive: box" material={{color: this.state.color}}
                 onClick={this.changeColor}
                 position="0 0 -5">
           <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
         </Entity>
+        // this.state.color insteaf of #999.
+        */}
+
+        {data.map(function(person) {
+          return <Entity key={person.id} data={person}
+                  geometry="primitive: box"
+                  material={{color: that.state.color}}
+                  onClick={that.changeColor}
+                  position="0 0 -5" >
+            <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+          </Entity>;
+        })}
       </Scene>
-    );
-  }
+    ); // render ()
+  } // render {}
 }
 
 ReactDOM.render(<BoilerplateScene/>, document.querySelector('.scene-container'));
