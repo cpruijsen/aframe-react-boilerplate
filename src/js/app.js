05e328301c2d7faaeb2c85cb0af1737d839b7ec8
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-07-09 22:22:04 -0700

    options for cylinder to include remainder of data
    data.length % (cylinder height * circle size)
    
    for example: dataset of 42, and I want 4 circles of 10 each stacked to make my cylinder, leaving 2 of my data
    
    -- now you can increase the size of one of your circles to render these 2

--- src/js/app.js
+++ src/js/app.js
@@ -20,8 +20,7 @@ class BoilerplateScene extends React.Component {
     super(props);
     this.state = {
       color: 'yellow',
-      layout: 'circle',
-      infoBoxVisible: false
+      layout: 'circle'
     }
   }
 
@@ -37,11 +36,6 @@ class BoilerplateScene extends React.Component {
     this.setState({
       layout: layoutTypes[Math.floor(Math.random() * layoutTypes.length)],
     });
-  };
-
-  changeInfoBoxVisible = () => { // toggles infoBoxVisible
-    this.setState({'infoBoxVisible': !this.state.infoBoxVisible});
-    // TODO move this to the model / for each box.
   };
 
   getLayoutOptions = () => {
@@ -93,18 +87,40 @@ class BoilerplateScene extends React.Component {
     var len = data.length;
     var datasq = Math.floor(Math.sqrt(len)); // 43 => 6
     var datacb = Math.floor(Math.cbrt(len));
+    var numCircles = Math.floor(len / 10); // arbitrary... for now.
     var dataRangeToSqrt = _.range(0, datasq + 1); // [0, 1, 2, 3, 4, 5, 6]
     var dataRangeToCbrt = _.range(0, datacb ); // 43 => [0, 1, 2, 3]
+    var dataRangeCircles = _.range(0, numCircles); // 42 => [0, 1, 2, 3]
 
     // circles and cylinders
     // need to provide an x and z value to position cylinder.
-    var circleIterator = datacb;
-    var circlePosition;
+    var circleIterator = numCircles;
+    var circlePosition, circleSliceEnd, circleSliceStart = -10;
     var changePosForCylinder = function(z) {   // change Y
       circleIterator--;
-      var position = dataRangeToCbrt[circleIterator];
+      var position = dataRangeCircles[circleIterator];
       circlePosition = `0 ${position} ${z}`;
     };
+    // there is no 'optimal' for circles unless we set it
+    // as you can make circle shapes with any number of primitives
+    // more circles just makes the outline more smooth
+
+    // could make a version that will always render *all* members of a dataset
+
+    var determineSlicingCylinder = function(options, i) {
+      if (options.all) { // naive - top circle is bigger.
+        circleSliceStart += 10;
+        if (i === numCircles - 1) { // this makes for very ugle cylinders.
+          circleSliceEnd = circleSliceStart + 10 + (len % 10);
+        } else {
+          circleSliceEnd = circleSliceStart + 10;
+        }
+      } else {
+        circleSliceStart += 10;
+        circleSliceEnd = circleSliceStart + 10;
+      }
+    };
+
     // rendering for cylinders can be cleaned up.
 
     return (
@@ -122,34 +138,29 @@ class BoilerplateScene extends React.Component {
             TODO: dynamic slicing
             TODO: onMouseEnter: () => {}, // show infobox
                   onMouseLeave: () => {}, // hide infobox
-
             infobox: curvedsurface? // pane with opacity and text?
-              name: name
-              bio: bio
-              Advanced: 'next' button on pane, transitions to page 2 with:
-              email: email
-              linkedin: linkedin
           */}
 
-          {dataRangeToCbrt.map(function(i) {
+          {dataRangeCircles.map(function(i) {
             changePosForCylinder(10);
+            determineSlicingCylinder({all: true}, i);
 
             return <Entity layout={{type: 'circle', radius: `${datacb}`}} position={circlePosition}>
             <Animation attribute="layout.radius" repeat="indefinite" to={`${datasq}`} direction="alternate" begin="2000"/>
 
-              {data.slice(0, 15).map(function(person) {
+              {data.slice(circleSliceStart, circleSliceEnd).map(function(person) {
+
                 return <Entity key={person.id} data={person}
                         geometry="primitive: box"
                         material={{src: `url(${person.image})`, color: that.state.color}}
-                        onClick={that.changeInfoBoxVisible}
-                        //onClick={that.changeColor}
+                        onClick={that.changeColor}
                         >
                         <Entity text={`text:  ${person.name}`}
                                 material="color: #66E1B4"
                                 scale="0.3 0.3 0.3"
                                 position="0 .5 -1"
                                 rotation="0 180 0"
-                                visible={that.state.infoBoxVisible} />
+                                visible="true" />
 
                 </Entity>; })}
 
